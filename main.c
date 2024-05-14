d25836eb5d45583aba29a0101f18f542a0df2df6
borgearthur <tubodish@gmail.com>
2024-05-14 08:57:31 -0300

    Initial creation of Libraries

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+/*
+ * Created by Arthur Borges and Raul Costa on 14/05/2024
+ * For a project in a grad class
+ * Based on tgfb/cli-lib
+ */
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "screen.h"
+#include "keyboard.h"
+#include "timer.h"
