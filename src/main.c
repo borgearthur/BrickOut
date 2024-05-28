110dbc250e561214cfa982d4d1c7bdf7ffa689a6
raulvnc <145472511+raulvnc@users.noreply.github.com>
2024-05-28 18:24:13 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,11 @@
   Para compilar os arquivos
 
   Escreva no terminal:
+  /*Primeiramente, certifique-se de que todas as bibliotecas necessárias e o arquivo main.c estejam no mesmo diretório em sua máquina local, conforme disponibilizado no GitHub. Em seguida, execute o comando a seguir:
+  */
+
+
+  
 
   1.   gcc -I../include keyboard.c main.c screen.c timer.c -o BrickOut
 
